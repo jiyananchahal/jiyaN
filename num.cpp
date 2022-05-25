c9b69a9ef522f59dff0d95503800891040f205b4
jiyananchahal <jiyananchahal@gmail.com>
2022-05-25 08:22:27 +0530

    number program

---
+++ num.cpp
@@ -0,0 +1,33 @@
+
+
+class arr
+{
+public:
+void create();
+void display();
+};
+void arr::create()
+{
+int i,n,flag=0,j;
+cout<<"enter the no. of elements in an array"<<endl;
+cin>>j;
+int a[j];
+cout<<"enter the "<<j<<" elements"<<endl;
+for(i=0;i<j;i++)
+{
+cin>>a[i];
+ }
+}
+void arr::display()
+{
+int flag=0,n,i,j,a[100];
+cout<<"enter no. to be checked"<<endl;
+cin>>n;
+for(i=0;i<j;i++)
+{
+if(a[i]==n)
+{
+flag=1;
+cout<<"index of no. is "<<i<<endl;
+break;
+PROBLEM STATEMENT :
